e9516f0ca2ab79f93c8173333f9b4d5ee90a12e8
Abineshdinu12 <abineshdinu@gmail.com>
2024-10-15 23:05:26 +0530

    abinesh-commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,25 @@
-import React from 'react';
-import Clients from '../components/Clients';
-import Cta from '../components/Cta';
-import Footer from '../components/Footer';
-import Hero from '../components/Hero';
-import Intro from '../components/Intro';
-import Portfolio from '../components/Portfolio';
-import Services from '../components/Services';
-import MediaPlayer from '../components/MediaPlayer';
-import Maps from '../components/Maps';
+import React from "react";
 
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Intro from "../components/Intro";
+
+import Services from "../components/Services";
+import MediaPlayer from "../components/MediaPlayer";
+import Maps from "../components/Maps";
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-            <Intro />
-            <Services />
-            {/* <Portfolio /> */}
-            {/* <Clients /> */}
-            
-            <MediaPlayer />
-            <Maps/>
-            
-            <Footer />
-        </>
+  return (
+    <>
+      <Hero />
+      <Intro />
+      <Services />
+      <MediaPlayer />
+      <Maps />
 
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
-
